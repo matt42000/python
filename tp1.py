8509164cafa11754acfa2e75956ff289d00b145d
matthieu <matt.vincenot@gmail.com>
2018-05-27 16:20:29 +0200

    tp1

--- tp1.py
+++ tp1.py
@@ -1,2 +1,23 @@
 # fonction input
-anne = input("saisie de l'année: ")
+year = input("saisie de l'année: ")
+print(year)
+print(type(year))
+#conversion string to int
+year = int(year)
+print(year)
+print(type(year))
+bisextile = False
+
+if year % 400 == 0:
+    bisextile = True
+elif year % 100 == 0:
+    bisextile = False
+elif year % 4 == 0:
+    bisextile = True
+else:
+    bisextile = False
+
+if bisextile:
+    print("l'année est bisextile")
+else:
+    print("l'année n'est pas bisextile")
